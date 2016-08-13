046f9571b4f5f3a5792051096fb4433da9440e41
soapnote <mmorgan@Mark-Morgans-MacBook.local>
2016-08-12 23:52:50 -0500

    adding 404 page

--- index.php
+++ index.php
@@ -26,63 +26,63 @@ include('lib/main-header.php');
 </div>
 <div class="container-fluid">
 	<div class="hidden-xs col-sm-3">
-		<a href="/cardiovascular/index.php">Cardiology</a>
+		<a style="font-weight:bold" href="/cardiovascular/index.php">Cardiology</a>
 		<?php include 'lib/menu-structure/cat-cardiovascular.php';?>
-		<a href="/skin/index.php">Dermatology</a>
+		<a style="font-weight:bold" href="/skin/index.php">Dermatology</a>
 		<?php include 'lib/menu-structure/cat-skin.php';?>
-		<a href="/endocrine-metabolic/index.php">Endocrine</a>
+		<a style="font-weight:bold" href="/endocrine-metabolic/index.php">Endocrine</a>
 		<?php include 'lib/menu-structure/cat-endocrine-metabolic.php';?>
-		<a href="/digestive-system/index.php">Gastroenterology</a>
+		<a style="font-weight:bold" href="/digestive-system/index.php">Gastroenterology</a>
 		<?php include 'lib/menu-structure/cat-digestive-system.php';?>
-		<a href="/general/index.php">General</a>
+		<a style="font-weight:bold" href="/general/index.php">General</a>
 		<?php include 'lib/menu-structure/cat-general.php';?>
-		<a href="/elder-care/index.php">Geriatrics</a>
+		<a style="font-weight:bold" href="/elder-care/index.php">Geriatrics</a>
 		<?php include 'lib/menu-structure/cat-elder-care.php';?>
-		<a href="/women/index.php">Gynecology</a>
+		<a style="font-weight:bold" href="/women/index.php">Gynecology</a>
 		<?php include 'lib/menu-structure/cat-women.php';?>
 	</div>
 	<div class="hidden-xs col-sm-3">
-		<a href="/blood-lymph/index.php">Hematology and Oncology</a>
+		<a style="font-weight:bold" href="/blood-lymph/index.php">Hematology and Oncology</a>
 		<?php include 'lib/menu-structure/cat-blood-lymph.php';?>
-		<a href="/infectious/index.php">Infectious Disease</a>
+		<a style="font-weight:bold" href="/infectious/index.php">Infectious Disease</a>
 		<?php include 'lib/menu-structure/cat-infectious.php';?>
-		<a href="/injury/index.php">Injury and Poisoning</a>
+		<a style="font-weight:bold" href="/injury/index.php">Injury and Poisoning</a>
 		<?php include 'lib/menu-structure/cat-injury.php';?>
-		<a href="/men/index.php">Men's Health</a>
+		<a style="font-weight:bold" href="/men/index.php">Men's Health</a>
 		<?php include 'lib/menu-structure/cat-men.php';?>
-		<a href="/musculoskeletal/index.php">Musculoskeletal</a>
+		<a style="font-weight:bold" href="/musculoskeletal/index.php">Musculoskeletal</a>
 		<?php include 'lib/menu-structure/cat-musculoskeletal.php';?>
-		<a href="/kidney/index.php">Nephrology</a>
+		<a style="font-weight:bold" href="/kidney/index.php">Nephrology</a>
 		<?php include 'lib/menu-structure/cat-kidney.php';?>
 	</div>
 	<div class="hidden-xs col-sm-3">
-		<a href="/neurological/index.php">Neurology</a>
+		<a style="font-weight:bold" href="/neurological/index.php">Neurology</a>
 		<?php include 'lib/menu-structure/cat-neurological.php';?>
-		<a href="/pregnancy/index.php">Obstetrics</a>
+		<a style="font-weight:bold" href="/pregnancy/index.php">Obstetrics</a>
 		<?php include 'lib/menu-structure/cat-pregnancy.php';?>
-		<a href="/eye/index.php">Ophthalmology</a>
+		<a style="font-weight:bold" href="/eye/index.php">Ophthalmology</a>
 		<?php include 'lib/menu-structure/cat-eye.php';?>
-		<a href="/ear-nose-throat/index.php">Otolaryngology</a>
+		<a style="font-weight:bold" href="/ear-nose-throat/index.php">Otolaryngology</a>
 		<?php include 'lib/menu-structure/cat-ear-nose-throat.php';?>
-		<a href="/patient-information/index.php">Patient Information</a>
+		<a style="font-weight:bold" href="/patient-information/index.php">Patient Information</a>
 		<?php include 'lib/menu-structure/cat-patient-information.php';?>
-		<a href="/child-health/index.php">Pediatrics</a>
+		<a style="font-weight:bold" href="/child-health/index.php">Pediatrics</a>
 		<?php include 'lib/menu-structure/cat-child-health.php';?>
 	</div>
 	<div class="hidden-xs col-sm-3">
-		<a href="/mental-health/index.php">Psychiatry</a>
+		<a style="font-weight:bold" href="/mental-health/index.php">Psychiatry</a>
 		<?php include 'lib/menu-structure/cat-mental-health.php';?>
-		<a href="/respiratory/index.php">Pulmonology</a>
+		<a style="font-weight:bold" href="/respiratory/index.php">Pulmonology</a>
 		<?php include 'lib/menu-structure/cat-respiratory.php';?>
-		<a href="/sample/index.php">Samples/Snippets</a>
+		<a style="font-weight:bold" href="/sample/index.php">Samples/Snippets</a>
 		<?php include 'lib/menu-structure/cat-sample.php';?>
-		<a href="/subjective/index.php">Subjective/History Elements</a>
+		<a style="font-weight:bold" href="/subjective/index.php">Subjective/History Elements</a>
 		<?php include 'lib/menu-structure/cat-subjective.php';?>			
-		<a href="/objective/index.php">Objective/Exam Elements</a>
+		<a style="font-weight:bold" href="/objective/index.php">Objective/Exam Elements</a>
 		<?php include 'lib/menu-structure/cat-objective.php';?>			
-		<a href="/assessment-plan/index.php">Assessment & Plan Elements</a>
+		<a style="font-weight:bold" href="/assessment-plan/index.php">Assessment & Plan Elements</a>
 		<?php include 'lib/menu-structure/cat-assessment-plan.php';?>			
-		<a href="/complete-note/index.php">Complete Notes</a>
+		<a style="font-weight:bold" href="/complete-note/index.php">Complete Notes</a>
 		<?php include 'lib/menu-structure/cat-complete-note.php';?>																														</div>
 </div>
 <?php
---
+++ whoops/index.php
@@ -0,0 +1,29 @@
+<?php 
+ include('../lib/main-header.php');
+?>
+<head><title>Page Not Found - SOAPnote.org</title></head>
+<ul class="breadcrumb">
+    <li><a href="/">Home</a></li>
+    <li class="active">Whoops</li>
+</ul>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>Whoops</h1>
+                <h2>Page Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occurred, the page you was looking for does not exist.
+                </div>
+                <div class="error-actions">
+                    <a href="http://www.soapnote.org" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                        Take Me Home </a><a href="mailto: postmaster@soapnote.org" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Webmaster </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<?php 
+ include('../lib/front-footer.php');
+?>	
