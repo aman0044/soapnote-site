2108886e668a8135c2e4684f23c2be31a3f515db
soapnote <mmorgan@Mark-Morgans-MacBook.local>
2016-08-23 05:54:54 -0500

    changed the title - hundreds (409 exactly) notes and calcs

--- lib/main-header.php
+++ lib/main-header.php
@@ -42,7 +42,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span> 
 			</button>	
-			<a class="navbar-brand" href="/">SOAPnote.org = medical notes + calcs</a>
+			<a class="navbar-brand" href="/">SOAPnote.org: Hundreds of Medical Notes & Calcs</a>
 		</div>
 		<div class="collapse navbar-collapse" id="myNavbar">
 			<ul class="nav navbar-nav">
