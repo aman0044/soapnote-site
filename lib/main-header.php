a6fcc8bcfbe0760bf9c62bfd6af62e017c78cde0
soapnote <mmorgan@Mark-Morgans-MacBook.local>
2016-08-27 08:03:37 -0500

    header change again

--- lib/main-header.php
+++ lib/main-header.php
@@ -44,7 +44,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span> 
 			</button>	
-			<a class="navbar-brand" href="/">SOAPnote.org:  Notes, Checklists, and Calculators</a>
+			<a class="navbar-brand" href="/">SOAPnote.org = Notes + Checklists + Calculators</a>
 		</div>
 		<div class="collapse navbar-collapse" id="myNavbar">
 			<ul class="nav navbar-nav">
