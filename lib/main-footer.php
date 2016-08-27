e9fb71807679861dc67f75ba014769236b0393d5
soapnote <mmorgan@Mark-Morgans-MacBook.local>
2016-08-27 07:58:59 -0500

    removing the disqus comment piece

--- lib/main-footer.php
+++ lib/main-footer.php
@@ -1,3 +1,4 @@
+<!-- 
 <div id="disqus_thread"></div>
 <script>
 /**
@@ -17,6 +18,7 @@ var disqus_config = function () {
 })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+ -->
 <footer>
 <div class="container-fluid">
 		<p class="text-muted">SOAPnote.org is an <a href="https://github.com/soapnote/soapnote-site" target="_blank">Open Source Project</a></p>
