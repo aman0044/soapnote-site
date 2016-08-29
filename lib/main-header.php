f6de575feeeea3263dac8b7255fab997a63cc879
soapnote <mmorgan@Mark-Morgans-MacBook.local>
2016-08-28 20:28:47 -0500

    update

--- lib/main-header.php
+++ lib/main-header.php
@@ -1,7 +1,10 @@
 <?php
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" manifest="soapnote.appcache">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<!-- 
+<html manifest="http://www.soapnote.org/soapnote.appcache">
+ -->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
